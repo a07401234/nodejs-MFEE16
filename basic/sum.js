1333b12297624c3a9f0cd35551d9f6f872eb8f9c
norman <a07401234@gmail.com>
2021-05-22 16:19:45 +0800

    sum

---
+++ basic/sum.js
@@ -0,0 +1,12 @@
+function sum(n) {
+    let num = 0;
+    for(i=1; i<=n; ++i)
+        num += i;
+    return num;
+  }
+  
+  console.log(sum(1)); // 1
+  console.log(sum(2)); // 3
+  console.log(sum(10)); // 55
+  console.log(sum(100000)); // 5000050000
+  
