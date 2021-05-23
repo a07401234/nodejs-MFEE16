a6ab53889cb20f08a3a44ab6ae5fad4185015f2b
norman <a07401234@gmail.com>
2021-05-23 14:36:11 +0800

    add quiz.js

---
+++ basic/quiz.js
@@ -0,0 +1,75 @@
+// (1) 請問下列程式執行後的結果為何？為什麼？
+
+// 1.start
+// 2.IIFE
+// 3.end
+// 4.Timeout  (因為有設定1秒延遲)
+
+// console.log("start");
+
+// (function () {
+//   console.log("IIFE");
+//   setTimeout(function () {
+//     console.log("Timeout");
+//   }, 1000);
+// })();
+
+// console.log("end");
+
+
+// (2) 請問下列程式執行的結果為何？為什麼？
+
+// 1.start
+// 2.IIFE
+// 3.end
+// 4.Timeout  (Javascript 是單一執行緒, 一次只做一件事, 會先把快的先完成?)
+
+// console.log("start");
+
+// (function () {
+//   console.log("IIFE");
+//   setTimeout(function () {
+//     console.log("Timeout");
+//   }, 0);
+// })();
+
+// console.log("end");
+
+
+// (3) 請問下列程式執行的結果為何？為什麼？
+
+// 1.foo
+// 2.bar
+// 3.baz
+
+// const bar = () => console.log("bar");
+
+// const baz = () => console.log("baz");
+
+// const foo = () => {
+//     console.log("foo");
+//     bar();
+//     baz();
+// };
+
+// foo();
+
+
+// (4) 請問下列程式執行的結果為何？為什麼？
+
+// 1.foo
+// 2.bar
+// 3.baz
+
+
+// const bar = () => console.log("bar");
+
+// const baz = () => console.log("baz");
+
+// const foo = () => {
+//     console.log("foo");
+//     setTimeout(bar, 0);
+//     baz();
+// };
+
+// foo();
