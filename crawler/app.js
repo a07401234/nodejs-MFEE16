6244cf79071066c2762fc2cd650c4a6ad427e383
norman <a07401234@gmail.com>
2021-05-29 12:25:43 +0800

    axios 0059

---
+++ crawler/app.js
@@ -0,0 +1,15 @@
+// https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20190101&stockNo=0059
+
+const axios = require('axios');
+
+// Make a request for a user with a given ID
+axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20190101&stockNo=0059')
+  .then(function (response) {
+    // handle success
+    // console.log(response.data.fields)
+    console.log(response.data.data)
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
