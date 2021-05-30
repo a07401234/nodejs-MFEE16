ea2d393038abc4a1282bfdd73ec33e83e798e2cd
norman <a07401234@gmail.com>
2021-05-30 10:42:32 +0800

    add xhr-promise

--- basic/sum.js
+++ basic/sum.js
@@ -1,14 +1,12 @@
 function sum(n) {
-    let num = 0;
-    for (i=0; i<=n; i++) {
-        num += i;
-    }
-    return num;
+  let num = 0;
+  for (i = 0; i <= n; i++) {
+    num += i;
   }
-  
-  console.log(sum(1)); // 1
-  console.log(sum(2)); // 3
-  console.log(sum(10)); // 55
-  console.log(sum(100000)); // 5000050000
-  
-  
+  return num;
+}
+
+console.log(sum(1)); // 1
+console.log(sum(2)); // 3
+console.log(sum(10)); // 55
+console.log(sum(100000)); // 5000050000
